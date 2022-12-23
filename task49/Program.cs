3666966a06750814cc4a7a8b5ef3f35a135c1a21
naumovanatalia <naumova.na43@mail.ru>
2022-12-23 18:39:03 +0200

    add task49

---
+++ task49/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matr;
+}
+
+// метод для печати (вывода в консоль) двумерного массива
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        
+    }
+}
+
+int[,] matrix = GetRandomMatrix(5, 5);
+PrintMatrix(matrix);
+for (int i = 2; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 2; j < matrix.GetLength(1); j +=2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+        
+    }
+Console.WriteLine();
+PrintMatrix(matrix);
