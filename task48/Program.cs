70d19899d3cf3753c6ca115ca7a7b944b97fd540
naumovanatalia <naumova.na43@mail.ru>
2022-12-23 18:23:03 +0200

    add task48

---
+++ task48/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// метод для создания двумерного массива
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i +j;
+        }
+    }
+    return matr;
+}
+
+// метод для печати (вывода в консоль) двумерного массива
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        
+    }
+}
+
+int rowsCount = ReadInt("Введите число строк");
+int columsCount = ReadInt("Введите число столбцов");
+int[,] matrix = GetRandomMatrix(rowsCount, columsCount);
+
+PrintMatrix(matrix);
